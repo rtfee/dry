bf460c735f1e9318054406e9c355b54214238ca2
Ryan <ryan.fee625@gmail.com>
2021-02-25 08:04:52 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+module "instance_module" {
+	source  = "scalr-demo.scalr.io/acc-sscctbisjkl35b8/instance_module/aws"
+	version = "0.0.3"
+
+	# Set 7 required variables below.
+
+	# AMI
+	ami = var.ami
+
+	# Instance Type
+	instance_type = var.instance_type
+
+	# AWS Key
+	key = var.key
+
+	# Region
+	region = var.region
+
+	# AWS Secruity Group
+	secgroup = var.sg
+
+	# Subnet ID
+	subnet = var.subnet
+
+	# VPC
+	vpc_id = var.vpc
+}
